2c28ca907460c88ceb0a751ca21fe2f622656af2
Dmitry <laitiskatel@ya.ru>
2016-05-12 19:19:33 +0500

    Third Impact
    Bye-bye, PHP (and XMLHttpRequest too).

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Description
-Fork of [JMR's Viewer Counter](https://www.reddit.com/r/Twitch/comments/2l5a28/showing_viewer_count_on_stream/clrodjn). See demo [here](https://rulait.ru/stuff/tvc/index.php?u=stopgameru&color=FFC0DB&size=40&interval=1).
+Fork of [JMR's Viewer Counter](https://www.reddit.com/r/Twitch/comments/2l5a28/showing_viewer_count_on_stream/clrodjn). See demo [here](https://rulait.github.io/twitch-viewer-counter/index.html?u=stopgameru&color=FFC0DB&size=40&interval=1).
 
-Script that getting JSON without jQuery was found [here](https://stackoverflow.com/questions/14388452/).
+Script for URL split was found [here](https://stackoverflow.com/a/979996).
 
 **License:** MIT License
 
@@ -12,11 +12,11 @@ Script that getting JSON without jQuery was found [here](https://stackoverflow.c
 * `&size=counter_font_size_in_pixels_here` (by default - 25)
 * `&interval=counter_update_interval_in_seconds_here` (by default - 5)
 
-For example - `domain.tld/tvc.php?u=stopgameru&color=FFC0DB&size=40&interval=1`
+For example - `domain.tld/tvc/index.html?u=stopgameru&color=FFC0DB&size=40&interval=1`
 
 ## Third party
 
 * [Twitch.tv](http://www.twitch.tv) icons
 
 ---
-ruLait, 2016. Contact - lait@cojam.ru.
+ruLait, 2016.
--- getdata.js
+++ getdata.js
@@ -1,38 +1,68 @@
-function GetJSONFile(path, callback) {
-	var httpRequest = new XMLHttpRequest();
-	httpRequest.onreadystatechange = function() {
-		if (httpRequest.readyState === 4) {
-			if (httpRequest.status === 200) {
-				var data = JSON.parse(httpRequest.responseText);
-				if (callback) callback(data);
-			}
-		}
-	};
-	httpRequest.open("GET", path);
-	httpRequest.send(); 
+"use strict";
+
+var params = {}, parts, nv, i;
+
+if (location.search) {
+	parts = location.search.substring(1).split('&');
+	for (i = 0; i < parts.length; i++) {
+		nv = parts[i].split('=');
+		if (!nv[0]) continue;
+		params[nv[0]] = nv[1] || true;
+	}
 }
 
-var counter = document.querySelector(".counter");
-var onlineicon = document.querySelector(".icon");
+var username, updInterval, fontcolor, size;
+
+if (params.u === undefined) {username = "twitch"} else {username = params.u}
+if (params.interval === undefined) {updInterval = "5000"} else {updInterval = params.interval + "000"}
+if (params.color === undefined) {fontcolor = "#000"} else {fontcolor = "#" + params.color}
+if (params.size === undefined) {size = "25px"} else {size = params.size + "px"}
+
+var counter = document.querySelector(".counter"),
+	onlineicon = document.querySelector(".icon"),
+	url = "https://api.twitch.tv/kraken/streams/" + encodeURIComponent(username) + "?from=tvc&ts=";
+
+function style(){
+	onlineicon.style.width = size;
+	onlineicon.style.height = size;
+	counter.style.fontSize = size;
+	counter.style.color = fontcolor;
+
+	// Just for debug
+	console.log("User: " + username + "\nCounter size: " + size + "\nFont color: " + fontcolor);
+}
 
 function loadViewers(){
-	GetJSONFile("https://api.twitch.tv/kraken/streams/" + encodeURIComponent(username) + "?from=tvc&ts=" + Date.now(), function(data){
-		if(data["stream"]!=null) {
-			if(data["streams"]) {
-				counter.textContent = "Channel is offline.";
+	if(self.fetch) {
+		window.fetch(url + Date.now()).then(function(response){
+			if (response.status !== 200) {
+				counter.textContent = "Twitch API error: " + response.status;
 				onlineicon.style.display = "none";
-			} else {
-				var viewer_count = data["stream"]["viewers"];
-				counter.textContent = viewer_count;
-				onlineicon.style.display = "inline-block";
+				return;
 			}
-		} else {
-			counter.textContent = "Channel is offline.";
-			onlineicon.style.display = "none";
-		}
-	});
+			response.json().then(function(data) {
+				if(data["stream"]!=null) {
+					if(data["streams"]) {
+						counter.textContent = "Channel is offline.";
+						onlineicon.style.display = "none";
+					} else {
+						var viewer_count = data["stream"]["viewers"];
+						counter.textContent = viewer_count;
+						onlineicon.style.display = "inline-block";
+					}
+				} else {
+					counter.textContent = "Channel is offline.";
+					onlineicon.style.display = "none";
+				}
+			});
+		});
+	} else {
+		counter.innerHTML = "Your browser doesn't support <a href='http://caniuse.com/#feat=fetch'>fetch</a>";
+		onlineicon.style.display = "none";
+	}
 }
 
+style();
 loadViewers();
 
 setInterval(function(){
